5b95734fac8c609baf7a9f6d1bd35fdd4a067d9c
Deoren Moor <deoren@users.noreply.github.com>
2018-01-29 02:44:37 -0600

    Update progit2-build-script.sh to use apk commands
    Giving this a try over the Ubuntu base images.

--- progit2/progit2-build-script.sh
+++ progit2/progit2-build-script.sh
@@ -1,28 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
-# Purpose: Script that is executed upon 'ubuntu' docker image launch
+# Purpose: Script that is executed upon docker image launch
 
 # Refs:
 #
 # * https://gist.github.com/deoren/e089f5f0698032d43452db4e0af003fc
+
+GIT_REPO="https://github.com/progit/progit2.git"
 
 #BRANCH="master"
 # Temp branch with Gemfile modified to use versions of gems that
 # are known to generate valid epub output.
 BRANCH="rework_files"
 
-REQ_PKGS="build-essential curl git-core ruby ruby-dev zlib1g-dev ruby-bundler"
+REQ_PKGS="git ruby ruby-dev alpine-sdk libxml2-dev zlib-dev"
 
-GIT_REPO="https://github.com/progit/progit2.git"
+apk update
+apk add --no-cache $REQ_PKGS
 
-apt-get update
-apt-get install -y $REQ_PKGS
+git clone $GIT_REPO
+cd progit2/
 
-git clone $GIT_REPO progit2
-cd progit2
-
+# Temp branch with Gemfile modified to use versions of gems that
+# are known to generate valid epub output.
 git checkout $BRANCH
 
+gem install bundler --no-ri --no-rdoc
+gem install rake --no-ri --no-rdoc
 bundle install --path vendor/bundle
 
 # Build all formats
