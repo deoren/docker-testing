ad813f7b350a7b6fdb1a516c51e23ccf945c63a5
Deoren Moor <deoren@users.noreply.github.com>
2018-01-31 17:20:32 -0600

    Create install-docker.sh
    Used to quickly automate the process of installing Docker CE on a test Ubuntu box.

---
+++ setup/install-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Purpose: 
+#
+#     Crude script to automate the installation of 
+#     the latest stable version of Docker CE for
+#     Ubuntu systems.
+
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+# Run pre-flight tests ...
+sudo docker run hello-world
